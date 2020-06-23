24d1e39b52d639db403eba63b767b2d75ef83ad4
Alexandre Cateli <48059013+catelii@users.noreply.github.com>
2020-06-22 23:04:31 -0300

    isso ai

---
+++ Assets/Scripts/Jogo/MoveOffset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffset : MonoBehaviour
+{
+    private Material currentMaterial;
+    public float speed;
+    private float offset;
+
+    void Start()
+    {
+        currentMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        offset += 0.001f;
+
+        currentMaterial.SetTextureOffset("_MainTex", new Vector2(offset * speed, 0));
+    }
+}
---
+++ Assets/Scripts/Jogo/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameObject player;
+    public float maxHeight;
+    public float minHeight;
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float tr = Input.GetAxis("Vertical") * speed;
+
+        player.transform.Translate(0, tr, 0);
+
+        if (player.transform.position.y > maxHeight)
+        {
+            player.transform.position = new Vector2(-6f, maxHeight);
+        }
+
+        if (player.transform.position.y < minHeight)
+        {
+            player.transform.position = new Vector2(-6f, minHeight);
+        }
+    }
+}
